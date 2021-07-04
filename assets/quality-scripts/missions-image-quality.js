3b7174214ce54e45c60370b535530010b3fb4d31
EddieHan <34524541+EddieHanGitHub@users.noreply.github.com>
2021-07-04 21:11:56 +1200

    Add files via upload

---
+++ assets/quality-scripts/missions-image-quality.js
@@ -0,0 +1,20 @@
+/* All of the code in here is a part of the quality system that Ethan Wales (l3) helped me to set up. 
+This system allows you to choose the quality of the image before they load, meaning that you have no broadband
+issues. */
+
+let imageQuality = localStorage.getItem("image-quality");
+
+if (imageQuality) {
+    if (imageQuality == "high") {
+        for (const itemOfArray in [...document.getElementsByClassName("Image")]) {
+            document.getElementsByClassName("Image")[itemOfArray].src = document.getElementsByClassName("Image")[
+                itemOfArray].src.slice(0, -4) + "High.png";
+        }
+    }
+    window.onload = _ => {
+        document.getElementById("loader").classList.add("hide");
+    }
+
+} else {
+    window.location.href = `./image-quality-changer.html?lastpage=${window.location.pathname.split("/").pop()}`
+}
